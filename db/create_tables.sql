5ab6695296c8c1fcd14c1717afb07c59ea8fd830
lapacek <simon.lapacek@gmail.com>
2022-04-09 23:25:27 +0200

    database schema - initial commit + code cleanup

---
+++ db/create_tables.sql
@@ -0,0 +1,14 @@
+CREATE SEQUENCE ticket_id_seq;
+
+CREATE TABLE IF NOT EXISTS ticket (
+    id integer NOT NULL DEFAULT nextval('ticket_id_seq'),
+    first_name varchar(200) NOT NULL,
+    last_name varchar(200) NOT NULL,
+    birthday TIMESTAMP NOT NULL,
+    launchpad_id INT NOT NULL,
+    destination_id INT NOT NULL,
+    launch_date TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
+    );
+
+ALTER SEQUENCE ticket_id_seq OWNED BY ticket.id;
