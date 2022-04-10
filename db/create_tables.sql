04ed12c1f1a7aa4283248db7d80036ee6d3070de
lapacek <simon.lapacek@gmail.com>
2022-04-10 10:55:56 +0200

    db normalization

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -2,13 +2,14 @@ CREATE SEQUENCE ticket_id_seq;
 
 CREATE TABLE IF NOT EXISTS ticket (
     id integer NOT NULL DEFAULT nextval('ticket_id_seq'),
+    launch_id INT NOT NULL,
     first_name varchar(200) NOT NULL,
     last_name varchar(200) NOT NULL,
-    birthday TIMESTAMP NOT NULL,
-    launchpad_id INT NOT NULL,
-    destination_id INT NOT NULL,
-    launch_date TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
+    birthday DATE NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_launch
+        FOREIGN KEY(launch_id)
+            REFERENCES launch(id)
     );
 
 ALTER SEQUENCE ticket_id_seq OWNED BY ticket.id;
@@ -17,13 +18,39 @@ CREATE SEQUENCE launch_id_seq;
 
 CREATE TABLE IF NOT EXISTS launch (
     id integer NOT NULL DEFAULT nextval('launch_id_seq'),
-    first_name varchar(200) NOT NULL,
-    last_name varchar(200) NOT NULL,
-    birthday TIMESTAMP NOT NULL,
     launchpad_id INT NOT NULL,
     destination_id INT NOT NULL,
-    launch_date TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
+    launch_date DATE NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_launchpad
+        FOREIGN KEY(launchpad_id)
+            REFERENCES launchpad(id),
+    CONSTRAINT fk_destination
+        FOREIGN KEY(destination_id)
+            REFERENCES destination(id)
     );
 
 ALTER SEQUENCE launch_id_seq OWNED BY launch.id;
+
+CREATE SEQUENCE destination_id_seq;
+
+CREATE TABLE IF NOT EXISTS destination (
+    id integer NOT NULL DEFAULT nextval('destination_id_seq'),
+    name varchar(200) NOT NULL,
+    PRIMARY KEY (id)
+    );
+
+ALTER SEQUENCE destination_id_seq OWNED BY destination.id;;
+
+CREATE SEQUENCE launchpad_id_seq;
+
+CREATE TABLE IF NOT EXISTS launchpad (
+    id integer NOT NULL DEFAULT nextval('launchpad_id_seq'),
+    id_spacex INT NOT NULL,
+    name varchar(200) NOT NULL,
+    PRIMARY KEY (id)
+    );
+
+ALTER SEQUENCE launchpad_id_seq OWNED BY launchpad.id;
+
+
